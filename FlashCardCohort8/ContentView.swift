d1fb28a4067901eb8dcb4b17d0a0520d2f28c704
kit sitou <ctwohofa@gmail.com>
2026-05-26 17:47:55 -0700

    Initial Commit

---
+++ FlashCardCohort8/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlashCardCohort8
+//
+//  Created by Kit Sitou on 5/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlashCardCohort8/FlashCardCohort8App.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardCohort8App.swift
+//  FlashCardCohort8
+//
+//  Created by Kit Sitou on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardCohort8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
